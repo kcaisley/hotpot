b98f054551a9fb2cfdc61df985114aca523a8f20
kcaisley <kcaisley@uni-bonn.de>
2026-09-21 08:41:45 +0200

    Update OpenROAD introduction and starter flow slides

--- flow.tcl
+++ flow.tcl
@@ -1,7 +1,5 @@
 set tech $env(OPENROAD_ROOT)/test/Nangate45
 set netlist $env(OPENROAD_ROOT)/test/gcd_nangate45.v
-set site FreePDK45_38x28_10R_NP_162NW_34O
-
 read_lef $tech/Nangate45_tech.lef
 read_lef $tech/Nangate45_stdcell.lef
 read_liberty $tech/Nangate45_typ.lib
